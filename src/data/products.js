ee5b62d76259a444fc6b643e70b577811b3c31fa
Samarit <kislyakaleksey@gmail.com>
2023-04-07 15:18:56 +0300

    added some product data

---
+++ src/data/products.js
@@ -0,0 +1,94 @@
+ const products = [
+  {
+    id: 1,
+    name: 'Краска Wallquest, Brownsone MS90101',
+    price: 6000,
+    tags: ['available', 'contracted']
+  },
+  {
+    id: 2,
+    name: 'Краска Wallquest, Brownsone MS90102',
+    price: 4800,
+    tags: ['new', 'available', 'exclusive']
+  },
+  {
+    id: 3,
+    name: 'Краска Wallquest, Brownsone MS90103',
+    price: 5290,
+    tags: ['new', 'contracted', 'sale']
+  },
+  {
+    id: 4,
+    name: 'Краска Wallquest, Brownsone MS90104',
+    price: 2800,
+    tags: ['available', 'contracted', 'exclusive', 'sale']
+  },
+  {
+    id: 5,
+    name: 'Краска Wallquest, Brownsone MS90105',
+    price: 3400,
+    tags: ['new', 'available', 'exclusive']
+  },
+  {
+    id: 6,
+    name: 'Краска Wallquest, Brownsone MS90106',
+    price: 6000,
+    tags: ['available', 'contracted', 'exclusive', 'sale']
+  },
+  {
+    id: 7,
+    name: 'Краска Wallquest, Brownsone MS90107',
+    price: 4800,
+    tags: ['new', 'available', 'contracted', 'exclusive',]
+  },
+  {
+    id: 8,
+    name: 'Краска Wallquest, Brownsone MS90108',
+    price: 5290,
+    tags: ['new', 'available', 'contracted', 'exclusive', 'sale']
+  },
+  {
+    id: 9,
+    name: 'Краска Wallquest, Brownsone MS90109',
+    price: 2800,
+    tags: ['new', 'available', 'contracted', 'exclusive', ]
+  },
+  {
+    id: 10,
+    name: 'Краска Wallquest, Brownsone MS901010',
+    price: 3400,
+    tags: ['contracted', 'exclusive', 'sale']
+  },
+  {
+    id: 11,
+    name: 'Краска Wallquest, Brownsone MS901011',
+    price: 6000,
+    tags: ['contracted', 'exclusive', 'sale']
+  },
+  {
+    id: 12,
+    name: 'Краска Wallquest, Brownsone MS901012',
+    price: 4800,
+    tags: ['available', 'exclusive', 'sale']
+  },
+  {
+    id: 13,
+    name: 'Краска Wallquest, Brownsone MS901013',
+    price: 5290,
+    tags: ['new', 'available', 'sale']
+  },
+  {
+    id: 14,
+    name: 'Краска Wallquest, Brownsone MS901014',
+    price: 2800,
+    tags: ['available', 'exclusive', 'sale']
+  },
+  {
+    id: 15,
+    name: 'Краска Wallquest, Brownsone MS901015',
+    price: 3400,
+    tags: ['contracted', 'exclusive', 'sale']
+  },
+]
+
+export default products
